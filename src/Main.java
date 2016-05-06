dd4579098e9bab8d6d2cad2d4332b54ad4cda32e
Kevin Vaysse <vayssek@gmail.com>
2016-05-06 15:08:22 +0200

    ADD
    ADD : modification in main method to create new directories from a
    scanner.²

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		// Scanner to enter a path to create an arboritory
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter a path :");
+		String str = new String (sc.nextLine());
+		Path dir = Paths.get(str);
+		boolean bool =Files.exists(dir);
+		if(bool==true){
+			System.out.println("The directory already exists");
+		}else{
+			try {
+				Path fold =Files.createDirectories(dir);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
 }
+}
+
+
